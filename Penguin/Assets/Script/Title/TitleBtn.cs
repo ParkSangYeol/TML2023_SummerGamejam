9de79b3eef3bbd38187d4a5592260d9825130e64
LeeJangHyun <dlwkdgus1114@ajou.ac.kr>
2023-08-30 09:23:05 +0900

    Feat: Other Panel
    조작법 및 튜토리얼 패널 제작

--- Penguin/Assets/Script/Title/TitleBtn.cs
+++ Penguin/Assets/Script/Title/TitleBtn.cs
@@ -8,10 +8,13 @@ public class TitleBtn : MonoBehaviour
 {
     [SerializeField] private AudioClip _mainMenuBGM;
     [SerializeField] private AudioClip _inGameBGM;
+
+    [SerializeField] private GameObject otherPanel;
     // Start is called before the first frame update
     void Start()
     {
         SFXManager.Instance.PlayLoop(_mainMenuBGM);
+        otherPanel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -24,5 +27,15 @@ public class TitleBtn : MonoBehaviour
         SceneManager.LoadScene("CombatScene");
         Time.timeScale = 1f;
         SFXManager.Instance.PlayLoop(_inGameBGM);
+    }
+
+    public void OnOtherPanel()
+    {
+        otherPanel.SetActive(true);
+    }
+
+    public void OffOtherPanel()
+    {
+        otherPanel.SetActive(false);
     }
 }
